c7e40cc8d7e4fc246e0ea561d3a6326ac38730a6
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-08-25 16:09:47 -0700

    Testing through the scripts. Displaying data.

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -7,8 +7,7 @@ var app = app || {};
 // Give the IIFE a parameter called 'module'.
 // At the very end of the code, but still inside the IIFE, attach the 'Article' object to 'module'.
 // Where the IIFE is invoked, pass in the global 'app' object that is defined above.
-(function(app){
-
+(function(module) {
   function Article(rawDataObj) {
     /* REVIEW: In lab 8, we explored a lot of new functionality going on here. Let's re-examine
     the concept of context.
@@ -38,7 +37,6 @@ var app = app || {};
 
   Article.loadAll = rows => {
     rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
-
     // DONE: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
     // is the transformation of one collection into another. Remember that we can set variables equal to the result
     // of functions. So if we set a variable equal to the result of a .map, it will be our transformed array.
@@ -49,91 +47,94 @@ var app = app || {};
     Article.all.push(new Article(ele));
   });
   */
-  let articleData = rawData.map();
-
+    rows.forEach(function(ele) {
+      Article.all.push(new Article(ele));
+    });
+    //let articleData = rawData.map();
+  }
 
   Article.fetchAll = callback => {
     $.get('/articles')
-  .then(
-    results => {
-      Article.loadAll(results);
-      callback();
-    }
-  )
+    .then(
+      results => {
+        Article.loadAll(results);
+        callback();
+      }
+    )
   };
 
-// TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+  // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
   Article.numWordsAll = () => {
     return Article.all.map(function(articleObj){
+      console.log(articleObj.body)
       return articleObj.body;
     }).reduce(function(previous, current){
-      let val = previous.length + current.length;
-      return val;
+      let val = previous.split(' ').length + current.split(' ').length;
     })
   };
 
-// TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
-// probably need to use the optional accumulator argument in your reduce call.
+  // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
+  // probably need to use the optional accumulator argument in your reduce call.
   Article.allAuthors = () => {
     return Article.all.map().reduce();
   };
 
   Article.numWordsByAuthor = () => {
-      return Article.allAuthors().map(author => {
-    // TODO: Transform each author string into an object with properties for
-    // the author's name, as well as the total number of words across all articles
-    // written by the specified author.
-    // HINT: This .map should be setup to return an object literal with two properties.
-    // The first property should be pretty straightforward, but you will need to chain
-    // some combination of filter, map, and reduce to get the value for the second
-    // property.
+    return Article.allAuthors().map(author => {
+      // TODO: Transform each author string into an object with properties for
+      // the author's name, as well as the total number of words across all articles
+      // written by the specified author.
+      // HINT: This .map should be setup to return an object literal with two properties.
+      // The first property should be pretty straightforward, but you will need to chain
+      // some combination of filter, map, and reduce to get the value for the second
+      // property.
 
-  })
-};
+    })
+  };
 
-Article.truncateTable = callback => {
-  $.ajax({
-    url: '/articles',
-    method: 'DELETE',
-  })
-  .then(console.log) // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
-  // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
-  // outside the scope of 301 material, but feel free to research!
-  .then(callback);
-};
+  Article.truncateTable = callback => {
+    $.ajax({
+      url: '/articles',
+      method: 'DELETE',
+    })
+    .then(console.log) // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
+    // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
+    // outside the scope of 301 material, but feel free to research!
+    .then(callback);
+  };
 
-Article.prototype.insertRecord = function(callback) {
-  // REVIEW: Why can't we use an arrow function here for .insertRecord()??
-  $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
-  .then(console.log)
-  .then(callback);
-};
+  Article.prototype.insertRecord = function(callback) {
+    // REVIEW: Why can't we use an arrow function here for .insertRecord()??
+    $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
+    .then(console.log)
+    .then(callback);
+  };
 
-Article.prototype.deleteRecord = function(callback) {
-  $.ajax({
-    url: `/articles/${this.article_id}`,
-    method: 'DELETE'
-  })
-  .then(console.log)
-  .then(callback);
-};
+  Article.prototype.deleteRecord = function(callback) {
+    $.ajax({
+      url: `/articles/${this.article_id}`,
+      method: 'DELETE'
+    })
+    .then(console.log)
+    .then(callback);
+  };
 
-Article.prototype.updateRecord = function(callback) {
-  $.ajax({
-    url: `/articles/${this.article_id}`,
-    method: 'PUT',
-    data: {
-      author: this.author,
-      authorUrl: this.authorUrl,
-      body: this.body,
-      category: this.category,
-      publishedOn: this.publishedOn,
-      title: this.title,
-      author_id: this.author_id
-    }
-  })
-  .then(console.log)
-  .then(callback);
-};
-  module.Article = Article;
-})(window);
+  Article.prototype.updateRecord = function(callback) {
+    $.ajax({
+      url: `/articles/${this.article_id}`,
+      method: 'PUT',
+      data: {
+        author: this.author,
+        authorUrl: this.authorUrl,
+        body: this.body,
+        category: this.category,
+        publishedOn: this.publishedOn,
+        title: this.title,
+        author_id: this.author_id
+      }
+    })
+    .then(console.log)
+    .then(callback);
+  };
+  app.Article = Article;
+})(app);
--- starter-code/server.js
+++ starter-code/server.js
@@ -8,7 +8,8 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://localhost:5432'; // DONE: Don't forget to set your own conString
+// const conString = 'postgres://localhost:5432'; // DONE: Don't forget to set your own conString
+const conString = 'postgres://postgres:password@localhost:5432/kilovolt'; // DONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', function(err) {
