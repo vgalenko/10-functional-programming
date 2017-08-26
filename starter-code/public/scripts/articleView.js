1c585f2ee4ffd0aa3499cfde856e06b85ed2521f
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-08-25 17:23:36 -0700

    Pushing for Friday end of day

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -138,10 +138,12 @@ var articleView = {};
     // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
     // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
     // we are then calling "template" on line 117.
+    var template = Handlebars.compile($('#admin-template').text());
 
     // REVIEW: We use `forEach` here because we are relying on the side-effects of the callback function:
     // appending to the DOM.
     // The callback is not required to return anything.
+    alert();
     app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
